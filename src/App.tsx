0ff5e547ff63945cf8dfed8d289011eb5d8739bb
malopez23 <malopez.rodriguez23@gmail.com>
2025-05-22 18:40:06 -0300

    Initial commit: Setup React + Vite + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">DesejosList 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
